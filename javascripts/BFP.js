3d02ec69b28b2bae2ac65c39735d9c5d10ef4d20
ordinarysoftware <nabdurasul@jacobs-university.de>
2023-12-20 21:24:22 +0100

    hi

---
+++ javascripts/BFP.js
@@ -0,0 +1,159 @@
+var BFP = (function () {
+  
+
+  var checkNodeNumber = 5;
+  var symbolNodeNumber = 10; 
+  var maxIterations= 4; 
+  
+  
+  var tannerGraph;
+  var hook;
+
+  
+  var calculateValues = function () {
+    // create an initial parity check matrix whose all elements are 0
+    // Given rows
+    const rows = [
+      "1111011000",
+      "0011111100",
+      "0101010111",
+      "1010100111",
+      "1100101011",
+    ];
+
+   
+
+    // Create a 5 by 10 matrix with initial values
+    var parityCheckMatrix = Array(checkNodeNumber)
+      .fill(null)
+      .map((_) => Array(symbolNodeNumber).fill(0));
+
+    // Populate the matrix with the given rows
+    rows.forEach((row, rowIndex) => {
+      row.split("").forEach((value, colIndex) => {
+        parityCheckMatrix[rowIndex][colIndex] = parseInt(value, 10);
+      });
+    });
+     
+    
+    
+     // Assuming there is a SymbolNode class with a method setValue(value)
+     
+     
+
+     for (let iteration = 0; iteration < maxIterations; iteration++) {
+      const updatedSymbolValues = [...symbolNodeValues];
+      updatecheckNodeValues=[];
+    
+      // Iterate over each check node
+      for (let checkNode = 0; checkNode < checkNodeNumber; checkNode++) {
+          // Find symbol nodes connected to the current check node
+          const connectedSymbolNodes = [];
+          for (let symbolNode = 0; symbolNode < symbolNodeNumber; symbolNode++) {
+              if (parityCheckMatrix[checkNode][symbolNode] === 1) {
+                  connectedSymbolNodes.push(symbolNode);
+              }
+          }
+          
+          
+
+          // if(checkNode===0){
+          //   tannerGraph = new TannerGraph(parityCheckMatrix);
+          //   hook && hook(tannerGraph);
+          // }
+          
+          console.log(connectedSymbolNodes);
+          console.log(updatedSymbolValues[checkNode], checkNode);
+
+          // Calculate the sum (mod 2) of the adjacent symbol node values
+          const sumMod2 = connectedSymbolNodes.reduce(
+              (sum, symbolNode) => (sum + symbolNodeValues[symbolNode]) % 2,
+              0
+          );
+
+
+          updatecheckNodeValues.push(sumMod2);
+         
+      }
+      for (let symbolNode = 0; symbolNode < symbolNodeNumber; symbolNode++) {
+        const connectedCheckNodes = [];
+        for (let checkNode = 0; checkNode < checkNodeNumber; checkNode++) {
+          if (parityCheckMatrix[checkNode][symbolNode] === 1) {
+            connectedCheckNodes.push(checkNode);
+          }
+      }
+      console.log(connectedCheckNodes);
+      const sumMod3=areValuesEqualAtIndexes(updatecheckNodeValues,connectedCheckNodes);
+      if(sumMod3===true){
+        symbolNodeValues[symbolNode]=updatedSymbolValues[connectedCheckNodes[0]]
+
+                
+      }
+      
+      }
+      
+
+      
+      
+      
+      tannerGraph = new TannerGraph(parityCheckMatrix);
+      hook && hook(tannerGraph);
+      checkNodeValue=updatecheckNodeValues;
+      
+      
+      console.log(`Iteratio:`);
+    console.log("Check Node Values:", checkNodeValue);
+    console.log("Symbol Node Values:", symbolNodeValues);
+    console.log("-----------------------------");
+    }
+    
+    
+    
+  };
+
+  return {
+    // creates the tanner-graph for the given parameters
+    create: function (options) {
+      
+      symbolNodeValues  = options.symbolNodeValues;
+      checkNodeValue= options.checkNodeValue;
+      
+
+      calculateValues();
+
+      return tannerGraph;
+    },
+
+    // this function registers a callback to be called
+
+    hook: function (callback) {
+      hook = callback;
+    },
+  };
+})();
+
+function areValuesEqualAtIndexes(array, indexes) {
+  const valuesToCheck = indexes.map(index => array[index]);
+  return valuesToCheck.every((value, index, arr) => value === arr[0]);
+}
+// for(let chi=0;chi<connectedSymbolNodes.length;chi++){
+//   var edgescheck=connectedSymbolNodes[chi];            
+//   updatedSymbolValues[edgescheck]=sumMod2;
+  
+  
+// }
+// for(let hi=0;hi<symbolNodeValues.length;hi++){
+//   var sample1=symbolNodeValues[hi];
+//   console.log(hi,updatedSymbolValues[hi]);
+  
+//   var sample2=updatedSymbolValues[hi];
+//   var sample3;
+//   if(sample1===sample2){
+//     console.log("same:",sample1,sample2);
+//     symbolNodeValues[hi]=sample2;
+//   }
+//   else{
+//     symbolNodeValues[hi]=sample1;
+//     console.log("not same:", sample1,sample2);
+//   }
+// }
---
+++ javascripts/main.js
@@ -0,0 +1,86 @@
+
+
+document.addEventListener('DOMContentLoaded', function(){
+  TannerGraph.vis = window.vis;
+
+  var createButton = document.querySelector('#create-button');
+  var nextButton = document.querySelector('#next-button');
+  var prevButton = document.querySelector('#prev-button');
+  var depthInput = document.querySelector('#depth-input');
+  var finalGraphContainer = document.querySelector('#final-graph');
+  var intermediateGraphContainer = document.querySelector('#intermediate-graph');
+  var subGraphContainer = document.querySelector('#sub-graph');
+  var finalMatrix = document.querySelector('#final-matrix');
+  var intermediateMatrix = document.querySelector('#intermediate-matrix');
+
+
+  var graphProgress = [];
+  var graphProgressIndex = 0;
+
+  createButton.addEventListener('click', function(event){
+    event.preventDefault();
+    
+    
+    var symbolNodeValues = document.querySelector('#symbol-node-values').value.split('').map(Number);
+    var checkNodeValue=[];
+    
+    
+    BFP.hook(function(intermediateGraph){
+      graphProgress.push(intermediateGraph.getClone());
+    });
+
+    graphProgress = [];
+    graphProgressIndex = 0;
+    var tannerGraph = BFP.create({
+      checkNodeValue: checkNodeValue,
+      symbolNodeValues: symbolNodeValues
+           
+    });
+
+    tannerGraph.render(finalGraphContainer);
+    finalMatrix.innerText = tannerGraph.matrix.map(a => a.join(' ')).join('\n');
+
+    graphProgress[0].render(intermediateGraphContainer);
+    intermediateMatrix.innerText = "";
+  });
+
+  nextButton.addEventListener('click', function(event){
+    event.preventDefault();
+    graphProgressIndex++;
+    if(graphProgressIndex >= graphProgress.length) graphProgressIndex = graphProgress.length-1;
+
+    var intermediateGraph = graphProgress[graphProgressIndex];
+    intermediateGraph.render(intermediateGraphContainer, function(data){
+      var subGraph = intermediateGraph.getSubGraph(data.nodes[0], 0);
+      subGraph.render(subGraphContainer);
+      
+      depthInput.value = 0;
+      depthInput.addEventListener('change', function(event){
+        var newSubGraph = intermediateGraph.getSubGraph(data.nodes[0], depthInput.value);
+        newSubGraph.render(subGraphContainer);
+      });
+    });
+    intermediateMatrix.innerText = intermediateGraph.matrix.map(a => a.join(' ')).join('\n');
+    document.getElementById("graphProgressIndex").textContent = graphProgressIndex;
+  });
+
+  prevButton.addEventListener('click', function(event){
+    event.preventDefault();
+    graphProgressIndex--;
+    if(graphProgressIndex < 0) graphProgressIndex = 0;
+
+    var intermediateGraph = graphProgress[graphProgressIndex];
+    intermediateGraph.render(intermediateGraphContainer, function(data){
+      var subGraph = intermediateGraph.getSubGraph(data.nodes[0], 0);
+      subGraph.render(subGraphContainer);
+
+      depthInput.value = 0;
+      depthInput.addEventListener('change', function(event){
+        var newSubGraph = intermediateGraph.getSubGraph(data.nodes[0], depthInput.value);
+        newSubGraph.render(subGraphContainer);
+      });
+    });
+    intermediateMatrix.innerText = intermediateGraph.matrix.map(a => a.join(' ')).join('\n');
+    document.getElementById("graphProgressIndex").textContent = graphProgressIndex;
+  });
+});
